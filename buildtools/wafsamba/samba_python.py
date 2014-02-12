bfb5057b6794e7360cd4d1b1550842a7db9970c2
Skerlet Development Team <skerlet@swc.toshiba.co.jp>
2014-02-12 15:34:09 +0900

    git-debimport tdb_1.2.10.orig.tar.gz

---
+++ buildtools/wafsamba/samba_python.py
@@ -0,0 +1,58 @@
+# waf build tool for building IDL files with pidl
+
+import Build
+from samba_utils import *
+from samba_autoconf import *
+
+from Configure import conf
+@conf
+def SAMBA_CHECK_PYTHON_HEADERS(conf, mandatory=True):
+    if conf.env["python_headers_checked"] == []:
+        conf.check_python_headers(mandatory)
+        conf.env["python_headers_checked"] = "yes"
+    else:
+        conf.msg("python headers", "using cache")
+
+
+def SAMBA_PYTHON(bld, name,
+                 source='',
+                 deps='',
+                 public_deps='',
+                 realname=None,
+                 cflags='',
+                 includes='',
+                 init_function_sentinal=None,
+                 local_include=True,
+                 vars=None,
+                 enabled=True):
+    '''build a python extension for Samba'''
+
+    # when we support static python modules we'll need to gather
+    # the list from all the SAMBA_PYTHON() targets
+    if init_function_sentinal is not None:
+        cflags += '-DSTATIC_LIBPYTHON_MODULES=%s' % init_function_sentinal
+
+    source = bld.EXPAND_VARIABLES(source, vars=vars)
+
+    if realname is not None:
+        link_name = 'python/%s' % realname
+    else:
+        link_name = None
+
+    bld.SAMBA_LIBRARY(name,
+                      source=source,
+                      deps=deps,
+                      public_deps=public_deps,
+                      includes=includes,
+                      cflags=cflags,
+                      local_include=local_include,
+                      vars=vars,
+                      realname=realname,
+                      link_name=link_name,
+                      pyext=True,
+                      target_type='PYTHON',
+                      install_path='${PYTHONARCHDIR}',
+                      allow_undefined_symbols=True,
+                      enabled=enabled)
+
+Build.BuildContext.SAMBA_PYTHON = SAMBA_PYTHON
